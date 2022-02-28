68ec811467dee47d84f03d32c57de1158e489b9c
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2022-02-28 13:47:27 -0500

    Update index.lua

--- sms/resources/install/scripts/app/sms/index.lua
+++ sms/resources/install/scripts/app/sms/index.lua
@@ -58,34 +58,6 @@
 		json = require "resources.functions.lunajson"
 	end
 
---define the urlencode function
-	local char_to_hex = function(c)
-		return string.format("%%%02X", string.byte(c))
-	end
-
-	local function urlencode(url)
-		if url == nil then
-			return
-		end
-		url = url:gsub("\n", "\r\n")
-		url = url.gsub(url, "([^%w _%%%-%.~])", char_to_hex)
-		url = url:gsub(" ", "+")
-		return url
-	end
-
-	local hex_to_char = function(x)
-	  return string.char(tonumber(x, 16))
-	end
-
-	local urldecode = function(url)
-	  if url == nil then
-	    return
-	  end
-	  url = url:gsub("+", " ")
-	  url = url:gsub("%%(%x%x)", hex_to_char)
-	  return url
-	end
-
 --define uuid function
 	local random = math.random;
 	local function uuid()
@@ -129,7 +101,6 @@
 		end
 		savebody = body;
 		body = body:gsub('<br>','\n');
-		body = urlencode(body);
 		
 		if (debug["info"]) then
 			freeswitch.consoleLog("notice", "[sms] TO: " .. to .. "\n");

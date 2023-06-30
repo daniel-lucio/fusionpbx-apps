57da1fde1a0724848249bebfe9cc8780f20f02fe
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2023-06-30 10:46:02 -0400

    Create sms_queue_distpacher.lua

---
+++ sms/resources/install/scripts/sms_queue_distpacher.lua
@@ -0,0 +1,8 @@
+requires "app.sms.resources.functions.send_outgoing";
+
+sms_message_uuid = argv[1];
+if (debug["info"]) then
+    freeswitch.consoleLog("notice", "[sms-distpacher] sms_message_uuid: " .. sms_message_uuid .. "\n");
+end
+
+send_outgoing(sms_message_uuid);

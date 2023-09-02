f94475f92a92dc56a70b348b0248dc589fa37b48
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2023-09-02 15:17:58 -0400

    Update index.lua

--- sms/resources/install/scripts/app/sms/index.lua
+++ sms/resources/install/scripts/app/sms/index.lua
@@ -632,9 +632,8 @@
 					outbound_delivery_method = settings['sms']['outbound_delivery_method']['text'] or 'direct';
 				end
 			end
-				if (settings['sms'][carrier..'_access_key']['text'] ~= nil) then
-					access_key = settings['sms'][carrier..'_access_key']['text']
-				end
+			if (settings['sms'][carrier..'_access_key']['text'] ~= nil) then
+				access_key = settings['sms'][carrier..'_access_key']['text']
 			end
 			if (settings['sms'][carrier..'_secret_key'] ~= nil) then
 				if (settings['sms'][carrier..'_secret_key']['text'] ~= nil) then
